b4b8d71185ea98c4a98988bdff30b6e07068f58c
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-10-03 18:37:52 +0530

    add apponiments model

---
+++ server/model/appoinment.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const appoinmentSchema = mongoose.Schema(
+  {
+    teacher: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    appointmentTime: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "canceled"],
+      default: "pending",
+    },
+    purpose: {
+      type: String,
+      trim: true,
+    },
+    feedback: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Appointment = mongoose.model("Appointment", appoinmentSchema);
+
+export default Appointment;
