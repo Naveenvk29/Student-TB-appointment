02610c57e8e79d9e9b1bf5e16826fcf68fa68d6b
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-10-04 00:32:50 +0530

    add rrd adn store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+//
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
+);
