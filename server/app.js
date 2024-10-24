67b39fdc76f597478be067169e8af7be79e9645d
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-10-24 16:48:35 +0530

    add cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
@@ -7,6 +8,8 @@ import messageRoutes from "./routes/message.route.js";
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
